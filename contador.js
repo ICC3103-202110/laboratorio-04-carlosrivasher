725838688ddd8e6e7b1502a5672df7c599056276
CarlosRivas <crivas1@miuandes.cl>
2021-05-04 02:21:08 -0300

    second commit

--- contador.js
+++ contador.js
@@ -3,3 +3,15 @@ function view(counter){
     (+) (-)\n
     (q)  for quit \n`)
 };
+
+function update(msg, counter){
+    if(msg=='+'){
+        return counter + 1
+    }
+    if(msg=='-'){
+        return counter - 1
+    }
+    else{
+        return counter
+    }
+};
