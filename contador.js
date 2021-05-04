5e13e5dbba67ee8895e51fb2e96dbdd7957472f7
CarlosRivas <crivas1@miuandes.cl>
2021-05-04 02:35:07 -0300

    third commit

--- contador.js
+++ contador.js
@@ -15,3 +15,15 @@ function update(msg, counter){
         return counter
     }
 };
+
+function app(counter){
+    while(true){
+        const currentView = view(counter);
+        console.clear();
+        console.log(currentView);
+        const msg = userInput
+        counter = update(msg, counter); 
+    }
+};
+
+app(0)
